ac6744cea560534309cb3edd9d12aebb7eac33ec
byrdie <byrdie@BATTLESTATION>
2014-01-24 09:37:35 -0700

    Need to figure out how strategy for extracting numbers from generic classes

--- 232_LAB_1/src/pkg232_lab_1/Element.java
+++ 232_LAB_1/src/pkg232_lab_1/Element.java
@@ -5,6 +5,34 @@ package pkg232_lab_1;
  *
  * @author byrdie
  */
-public class Element {
+abstract class Element<K, V> implements Pair<K, V> {
+    private int num;
+    private String string;
     
+    private K key;
+    private V value;
+    
+    
+    Element(int i, String s){
+        this.num = i;
+        this.string = s;
+    }
+    
+    public int compareTo(Object element){
+        Element otherElement = (Element) element;
+
+        if(string.compareTo(otherElement.string) < 0){
+            return -1;
+        }else if(string.compareTo(otherElement.string) == 0){
+            if(num < otherElement.num){
+                return -1;
+            }else if(num == otherElement.num){
+                return 0;
+            }else{
+                return 1;
+            }
+        }else{
+            return 1;
+        }
+    }
 }
--- 232_LAB_1/src/pkg232_lab_1/Tree.java
+++ 232_LAB_1/src/pkg232_lab_1/Tree.java
@@ -5,7 +5,7 @@ package pkg232_lab_1;
  *
  * @author Roy
  */
-public class Tree <E extends Comparable>{
+public class Tree <E extends Comparable> implements Pair<K,V>{
     private Node <E> root;
     
     Tree (E data){
@@ -15,11 +15,29 @@ public class Tree <E extends Comparable>{
         root = temp;
     }
     
-    public boolean insert(Node<E> data, Node<E> root){
-        if(root == null){
-            root  = data;
-        }else if(root.getData().){
-            
+    public boolean insert(Node<E> data, Node<E> branchRoot){
+        if(branchRoot == null){
+            branchRoot  = data;
+            return true;
+        }else if((data.getData().compareTo(branchRoot.getData())) == 0){
+            return false;
+        }
+        else if((data.getData().compareTo(branchRoot.getData())) < 0){
+            insert(data, branchRoot.getLeft());
+        }
+        else{
+            insert(data, branchRoot.getRight());
+        }
+        return false;
+    }
+    
+    void inorder(Node<E> node){
+        if (node == null){
+            return;
+        }
+        else{
+            inorder(node.getLeft());
+            node.getData().
         }
     }
 }
